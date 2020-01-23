765a4065bbbe747ffdf1700cd7256fa1a22bac40
Vincent <bluekiller57@gmail.com>
2020-01-22 19:38:38 -0500

    implemented nested mappings

--- src/main/java/com/api/newscompilerapi/controllers/NewsController.java
+++ src/main/java/com/api/newscompilerapi/controllers/NewsController.java
@@ -1,17 +1,19 @@
 package com.api.newscompilerapi.controllers;
 
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@RequestMapping("/api")
 public class NewsController {
 
-    @RequestMapping("/search")
+    @RequestMapping(value="/search", method= RequestMethod.GET)
     public String index() {
         return "Greetings from Spring Boot!";
     }
 
-    @RequestMapping("/anna")
+    @RequestMapping(value="/anna", method=RequestMethod.GET)
     public String f2() {
         return "Greetings from Spring Anna Boot!";
     }
