69c4d057e8326fb197832824542329b44cbbea09
Anna Liu <annaliu1210@gmail.com>
2020-01-22 22:23:53 -0500

    Added JSON for NYPost

--- src/main/java/com/api/newscompilerapi/controllers/NewsController.java
+++ src/main/java/com/api/newscompilerapi/controllers/NewsController.java
@@ -1,5 +1,6 @@
 package com.api.newscompilerapi.controllers;
 
+import com.api.newscompilerapi.HtmlParser.NYPostSearchParser;
 import com.api.newscompilerapi.HtmlParser.NYTimesSearchParser;
 import com.api.newscompilerapi.models.Article;
 import org.json.JSONArray;
@@ -18,15 +19,26 @@ public class NewsController {
 
     @RequestMapping(value="/search", method= RequestMethod.GET)
     public String getArticles(@RequestParam("query") String query) throws IOException {
-        NYTimesSearchParser parser = new NYTimesSearchParser();
-        List<Article> articles = parser.parseForArticles(query);
         JSONArray articlesJSON = new JSONArray();
-        for (Article article : articles) {
+
+        NYTimesSearchParser nYTimesParser = new NYTimesSearchParser();
+        List<Article> nYTimesArticles = nYTimesParser.parseForArticles(query);
+        for (Article article : nYTimesArticles) {
             JSONObject articleJSON = new JSONObject();
             articleJSON.put("url", article.getUrl());
             articleJSON.put("title", article.getTitle());
             articlesJSON.put(articleJSON);
         }
+
+        NYPostSearchParser nYPostParser = new NYPostSearchParser();
+        List<Article> nYPostArticles = nYPostParser.parseForArticles(query);
+        for (Article article : nYPostArticles) {
+            JSONObject articleJSON = new JSONObject();
+            articleJSON.put("url", article.getUrl());
+            articleJSON.put("title", article.getTitle());
+            articlesJSON.put(articleJSON);
+        }
+        
         return articlesJSON.toString();
     }
 
